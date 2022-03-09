8bd0dcf8b596c11cb9d7082314d9c1113fcf584c
NadielySilva <nady.ceng@gmail.com>
2022-03-09 19:26:56 -0300

    estilizando

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
-// import styled from 'styled-components'
-
-// const myBotton = styled.button
-// `
-//   background: transparent;
-//   border-radius: 3px;
-//   border: 2px solid palevioletred;
-//   color: palevioletred;
-//   margin: 0 1em;
-//   padding: 0.25em 1em;
-// `
-
+import styled from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 //Importando Assets
 import myBackground from "./Assets/the witcher hell.png"
@@ -18,19 +8,62 @@ import myLogo from "./Assets/witcher simbol.png"
 import myWitcher from "./Assets/the witcher kavil2.png"
 import myShowName from "./Assets/the witcher.jpeg.png"
 
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap');  
+*{
+  font-family: 'Montserrat', sans-serif;
+  text-transform: uppercase;
+  margin:0;
+  padding:0;
+  list-style-type: none;
+  box-sizing:border-box;
+}
+
+header{
+  width: 100vw;
+  height: 10vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+ul{
+  width: 50vw;
+  height: 10vh;
+  display: flex;
+  flexdirections: row;
+  align-items: center;
+  justify-content: space-around;
+}
+`
+const Logo = styled.div`
+width: 30vw;
+height: 10vh;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+`
+
+const MyLogoImage = styled.img`
+width: 3vw;
+height: auto;
+`
 
 export default class App extends Component{
 
   render(){
     return(
       <div>
+        <GlobalStyle/>
         <header>
-          <div>
+          <Logo>
             <figure>
-              <img src={myLogo} alt="Logo da série 'The Witcher' feito da ponta de um tridente vermelho formando um 'W'" />
+              <MyLogoImage src={myLogo} alt="Logo da série 'The Witcher' feito da ponta de um tridente vermelho formando um 'W'" />
             </figure>
             <h2>Projeto Vermelho</h2>
-          </div>
+          </Logo>
           <nav>
             <ul>
               <li>Universo</li>
@@ -45,7 +78,7 @@ export default class App extends Component{
             <img src={myWitcher} alt="Personagem 'The Witcher' em armadura completa com duas espadas nas costas e o que parece ser um machado em uma de suas mãos. O bruxo tem olhos verde-amarelados e uma grande cicatriz que vai do lado esquerdo da testa, passa pelo olho esquerdo, e para no meio do rosto do lado esquerdo."/>
             <img src={myShowName} alt="Nome da série 'The Witcher', mais o subtítulo 'Wild Hunt' acompanhada do logo 'W'."/>
           </figure>
-          <myBotton>Assista Agora</myBotton>
+          <button>Assista Agora</button>
         </main>
         <footer>
           <p>
